ef6afa50fc2b79f71b6570bde59503d39a49edca
suraj mirajkar <surajmirajkar@rediffmail.com>
2012-11-30 18:51:30 +0530

    Added upload and delete functionality to DropBox iOS integration sample code

---
+++ dropbox-ios-sdk-1.3.2/examples/DBRoulette/Classes/PhotoViewController.h
@@ -0,0 +1,28 @@
+//
+//  PhotoViewController.h
+//  DBRoulette
+//
+
+
+@class DBRestClient;
+
+@interface PhotoViewController : UIViewController {
+    UIImageView* imageView;
+    UIButton* nextButton;
+    UIActivityIndicatorView* activityIndicator;
+    
+    NSArray* photoPaths;
+    NSString* photosHash;
+    NSString* currentPhotoPath;
+    BOOL working;
+    DBRestClient* restClient;
+}
+
+@property (nonatomic, retain) IBOutlet UIImageView* imageView;
+@property (nonatomic, retain) IBOutlet UIButton* nextButton;
+@property (nonatomic, retain) IBOutlet UIActivityIndicatorView* activityIndicator;
+
+- (IBAction)uploadToDropBox:(id)sender;
+- (IBAction)deleteFromDropBox:(id)sender;
+
+@end
